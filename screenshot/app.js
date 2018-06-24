eca5f2e3e56530666415b8ef5cc3d63374cc0f7c
Damian Cipolat <damian.cipolat@gmail.com>
2018-06-24 18:13:26 -0300

    subo

---
+++ screenshot/app.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+
+
+const run = ()=>{
+
+  //Paths and urls.
+  const url          = 'https://github.com';
+  const pathDownload = './github.png';
+
+  //Start puppeteer.
+  const bot = puppeteer.launch({headless:true,args:['--no-sandbox','--disable-setuid-sandbox']});
+
+  //When the browser open.
+  bot.then((browser)=>{   
+
+    //Create a new page. 
+    browser.newPage().then((page)=>{
+
+      //Navigate to github.
+      page.goto(url).then((content)=>{
+
+        //Take a snapshoot.
+        page.screenshot({path:pathDownload}).then((result)=>{
+
+          console.log('snapshoot ok!');
+          process.exit();
+
+        });
+
+      });
+
+    });
+
+  }).catch((err)=>console.log('err',err));
+
+
+}
+
+run();
